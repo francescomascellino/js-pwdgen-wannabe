240e35d09a1281afe465afdbdf4bcd839d04ee8c
francescomascellino <franesco.mascellino@gmail.com>
2023-08-22 16:08:04 +0200

    add random number generator from 0 to 999

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,19 +1,21 @@
-let userName
-let userSurname
-let userFavColor
-const button = document.getElementById("pwdGenerator")
+let userName;
+let userSurname;
+let userFavColor;
+let pwdNum;
+const button = document.getElementById("pwdGenerator");
 
 button.addEventListener("click", function generatePwd (userName, userSurname, userFavColor) {
 
     userName = prompt("STEP 1: Inserisci il tuo nome")
     userSurname = prompt("STEP 2: Inserisci il tuo cognome")
-    userFavColor = prompt("STEP 3: Inserisci il tuo colore preferito")    
+    userFavColor = prompt("STEP 3: Inserisci il tuo colore preferito")
+    pwdNum = Math.floor(Math.random() * 999);
     
-    console.log("La password generata è ", userName + userSurname + userFavColor + 23);
+    console.log("La password generata è ", userName + userSurname + userFavColor + pwdNum);
     
     document.getElementById("callToAction").innerHTML = "La tua nuova e sicurissima password è:";
 
-    document.getElementById("pwdField").innerHTML = userName + userSurname + userFavColor + 23;
+    document.getElementById("pwdField").innerHTML = userName + userSurname + userFavColor + pwdNum;
 
     button.innerHTML = "Clicca per generare una nuova password";
 
