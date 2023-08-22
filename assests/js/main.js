0973d69eaff03e447b105e5bcd7df197e0d0fc9b
francescomascellino <franesco.mascellino@gmail.com>
2023-08-22 14:38:20 +0200

    changed button text on pws generation

--- assests/js/main.js
+++ assests/js/main.js
@@ -14,17 +14,9 @@ button.addEventListener("click", function generatePwd (userName, userSurname, us
     console.log("La password generata è ", userName + userSurname + userFavColor + 23);
     
     document.getElementById("callToAction").innerHTML = "La tua nuova e sicurissima password è:";
+
     document.getElementById("pwdField").innerHTML = userName + userSurname + userFavColor + 23;
+
+    button.innerHTML = "Clicca per generare una nuova password";
+
     });
-
-/* function generatePwd (userName, userSurname, userFavColor) {
-
-userName = prompt("STEP 1: Inserisci il tuo nome")
-userSurname = prompt("STEP 2: Inserisci il tuo cognome")
-userFavColor = prompt("STEP 3: Inserisci il tuo colore preferito")    
-
-console.log("La password generata è ", userName + userSurname + userFavColor + 23);
-
-document.getElementById("callToAction").innerHTML = "La tua nuova e sicurissima password è:";
-document.getElementById("pwdField").innerHTML = userName + userSurname + userFavColor + 23;
-} */
