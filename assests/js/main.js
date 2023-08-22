cf95d0ebaf2319a96c482f43ff1fdcc77a0a40bf
francescomascellino <franesco.mascellino@gmail.com>
2023-08-22 14:35:15 +0200

    fixed function

--- assests/js/main.js
+++ assests/js/main.js
@@ -3,14 +3,28 @@ let userSurname
 let userFavColor
 const button = document.getElementById("pwdGenerator")
 
-function generatePwd (userName, userSurname, userFavColor) {
+
+
+button.addEventListener("click", function generatePwd (userName, userSurname, userFavColor) {
+
+    userName = prompt("STEP 1: Inserisci il tuo nome")
+    userSurname = prompt("STEP 2: Inserisci il tuo cognome")
+    userFavColor = prompt("STEP 3: Inserisci il tuo colore preferito")    
+    
+    console.log("La password generata è ", userName + userSurname + userFavColor + 23);
+    
+    document.getElementById("callToAction").innerHTML = "La tua nuova e sicurissima password è:";
+    document.getElementById("pwdField").innerHTML = userName + userSurname + userFavColor + 23;
+    });
+
+/* function generatePwd (userName, userSurname, userFavColor) {
 
 userName = prompt("STEP 1: Inserisci il tuo nome")
 userSurname = prompt("STEP 2: Inserisci il tuo cognome")
 userFavColor = prompt("STEP 3: Inserisci il tuo colore preferito")    
 
+console.log("La password generata è ", userName + userSurname + userFavColor + 23);
+
 document.getElementById("callToAction").innerHTML = "La tua nuova e sicurissima password è:";
 document.getElementById("pwdField").innerHTML = userName + userSurname + userFavColor + 23;
-}
-
-generatePwd (userName, userSurname, userFavColor);
+} */
